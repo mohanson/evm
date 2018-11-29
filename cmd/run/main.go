2006d4e1dc36802dbe584d5e7a6f08cc72c4a3b7
mohanson <mohanson@outlook.com>
2018-11-29 23:06:33 +0800

    2018-11-29 23:06:33

---
+++ cmd/run/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/vm/runtime"
+)
+
+func main() {
+	ret, _, err := runtime.Execute(common.Hex2Bytes("6060604052600a8060106000396000f360606040526008565b00"), nil, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(ret)
+	// Output:
+	// [96 96 96 64 82 96 8 86 91 0]
+}
